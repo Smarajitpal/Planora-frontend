61f96e2e7a41b635a5974f9e9066103e30992525
Smarajit Pal <smarajitpal@Smarajits-MacBook-Pro.local>
2025-07-09 23:43:50 +0530

    login

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -1,5 +1,5 @@
 import Modal from "react-bootstrap/Modal";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userLoginAsync } from "../features/User/userSlice";
